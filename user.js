ca56bea827516f727bba1d54647baf4dd7c7f34b
Jonathan Lowell <elderlowell@gmail.com>
2017-06-27 20:44:29 -0600

    Completed Step 2.

---
+++ user.js
@@ -0,0 +1,60 @@
+var user = {
+  name: "Jonathan Lowell",
+  location: "Utah",
+  occupations: ["attorney", "programmer"],
+  hobbies: [
+    {
+      name: "coding",
+      type: "tech"
+    },
+    {
+      name: "basketball",
+      type: "sports"
+    },
+    {
+      name: "playing with children",
+      type: "social"
+    }
+  ],
+  family: [
+    {
+      name: "Jennifer",
+      relation: "wife",
+      gender: "female"
+    },
+    {
+      name: "Emma",
+      relation: "daughter",
+      gender: "female"
+    },{
+      name: "Andrew",
+      relation: "son",
+      gender: "male"
+    },{
+      name: "Brinley",
+      relation: "daughter",
+      gender: "female"
+    },{
+      name: "Aubrey",
+      relation: "daughter",
+      gender: "female"
+    }
+  ],
+  restaurants: [
+    {
+      name: "Even Steven",
+      type: "sandwiches",
+      rating: 4
+    },
+    {
+      name: "R & R BBQ",
+      type: "barbeque",
+      rating: 4
+    },
+    {
+      name: "Cafe Rio",
+      type: "mexican",
+      rating: 4
+    }
+  ]};
+module.exports = user;
